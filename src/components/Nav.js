694768fe6f1897a5339b3b6e3732485891813b11
Martypose <46140279+Martypose@users.noreply.github.com>
2020-08-10 12:54:28 +0200

    yes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,6 +14,9 @@ function Nav() {
             <Link to='/Medidas'>
                 <li>Medidas</li>
             </Link>
+            <Link to='/Tranportistas'>
+                <li>Transportistas</li>
+            </Link>
         </ul>
     </nav>
   );
