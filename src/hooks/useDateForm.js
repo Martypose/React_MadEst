7269cf7acfecfc692f7259180e227546673dc6a7
Martypose <martinpose@hotmail.com>
2023-10-30 15:53:12 +0100

    go

--- src/hooks/useDateForm.js
+++ src/hooks/useDateForm.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 const useDateForm = () => {
   const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
   const lastMonth = new Date(today);
   lastMonth.setMonth(today.getMonth() - 1);
 
   const [startDate, setStartDate] = useState(lastMonth);
-  const [endDate, setEndDate] = useState(today.getDay + 1);
+  const [endDate, setEndDate] = useState(tomorrow);
 
   return {
     startDate,
