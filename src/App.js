fc5a8466cbcdc10a9134eb34a41b8596b0ddce90
Martypose <martinpose@hotmail.com>
2023-11-08 22:15:58 +0100

    better error management and fix error MenuItem

--- src/App.js
+++ src/App.js
@@ -9,63 +9,79 @@ import Login from "./components/Login";
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_URL_API;
 
+// Configuración global de axios
+axios.defaults.baseURL = process.env.REACT_APP_URL_API;
+
+// Interceptor de axios para manejar la renovación de tokens y errores de autenticación
 axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
+  (response) => response, // Si la respuesta es exitosa, simplemente la retornamos.
   (error) => {
-    console.log("Error en llamada, comprobando cual es...");
-    console.log(error.response);
-    const originalRequest = error.config;
-    if (error.response.status === 301 && !originalRequest._retry) {
-      originalRequest._retry = true;
+    const { status } = error.response;
+    if (status === 401) {
+      // Si el token de acceso ha expirado o es inválido
+      clearLocalStorage();
+      redirectToLogin();
+    } else if (status === 301 && !error.config._retry) {
+      // Manejo de redirección, por ejemplo, podría ser un cambio de URL
+      error.config._retry = true;
       return axios.request(error.config);
+    } else if (status === 402 && !error.config._retry) {
+      // Si el servidor retorna 402, podría ser un indicativo de que es necesario refrescar el token
+      error.config._retry = true;
+      return refreshAccessToken(error.config);
     }
-    if (error.response.status === 402 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      return axios
-        .get(`${process.env.REACT_APP_URL_API}/refreshtoken`, {
-          headers: {
-            "Content-Type": "application/json",
-            refreshToken: localStorage.getItem("refreshToken"),
-            username: localStorage.getItem("username"),
-          },
-        })
-        .then((response) => {
-          console.log("pidiendo accesToken con refresh");
-          if ("accessToken" in response.data) {
-            console.log("me sirven los tokens");
-            localStorage.setItem(
-              "accessToken",
-              response.data["accessToken"].accessToken
-            );
-            console.log(
-              "nuevo tokenaccess: " + response.data["accessToken"].accessToken
-            );
-            localStorage.setItem(
-              "refreshToken",
-              response.data["refreshToken"].refreshToken
-            );
-            console.log(
-              "nuevo refresh: " + response.data["refreshToken"].refreshToken
-            );
-            localStorage.setItem("username", response.data["username"]);
-            error.config.headers.accessToken =
-              localStorage.getItem("accessToken");
-            return axios.request(error.config);
-          } else {
-            localStorage.removeItem("refreshToken");
-            localStorage.removeItem("accessToken");
-            localStorage.removeItem("username");
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
+    // Para otros tipos de errores, simplemente los rechazamos.
     return Promise.reject(error);
   }
 );
+
+// Función para limpiar el localStorage
+const clearLocalStorage = () => {
+  localStorage.removeItem("refreshToken");
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("username");
+};
+
+// Función para redirigir al Login
+const redirectToLogin = () => {
+  window.location = "/login";
+};
+
+// Función para refrescar el accessToken
+const refreshAccessToken = (originalRequest) => {
+  return axios
+    .get(`${process.env.REACT_APP_URL_API}/refreshtoken`, {
+      headers: {
+        "Content-Type": "application/json",
+        refreshToken: localStorage.getItem("refreshToken"),
+        username: localStorage.getItem("username"),
+      },
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem(
+          "accessToken",
+          response.data.accessToken.accessToken
+        );
+        localStorage.setItem(
+          "refreshToken",
+          response.data.refreshToken.refreshToken
+        );
+        localStorage.setItem("username", response.data.username);
+        originalRequest.headers.accessToken =
+          localStorage.getItem("accessToken");
+        return axios.request(originalRequest);
+      } else {
+        clearLocalStorage();
+        redirectToLogin();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      clearLocalStorage();
+      redirectToLogin();
+    });
+};
 
 function App() {
   const token = localStorage.getItem("accessToken");
@@ -73,6 +89,7 @@ function App() {
   if (!token) {
     return <Login />;
   }
+
   return (
     <div className="App">
       <Router>
@@ -81,6 +98,7 @@ function App() {
           <Route path="/estadisticas" exact component={Estadisticas} />
           <Route path="/cubico" component={AnalisisProduccion} />
           <Route path="/tablasnumero" component={TablasNumeros} />
+          {/* Otras rutas y componentes */}
         </Switch>
       </Router>
     </div>
--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,45 +1,47 @@
 import Dropdown from "../utils/Dropdown";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 
-
 const MenuItems = ({ items }) => {
+  const [dropdown, setDropdown] = useState(false);
+  let ref = useRef();
 
-    const [dropdown, setDropdown] = useState(false);
-    let ref = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler);
+    };
+  }, [dropdown]);
 
-    useEffect(() => {
-        const handler = (event) => {
-         if (dropdown && ref.current && !ref.current.contains(event.target)) {
-          setDropdown(false);
-         }
-        };
-        document.addEventListener("mousedown", handler);
-        document.addEventListener("touchstart", handler);
-        return () => {
-         // Cleanup the event listener
-         document.removeEventListener("mousedown", handler);
-         document.removeEventListener("touchstart", handler);
-        };
-       }, [dropdown]);
-       
- return (
-  <li className="menu-items" ref={ref}>
-   {items.submenu ? (
-    <>
-     <button aria-expanded={dropdown ? "true" : "false"}
-      onClick={() => setDropdown((prev) => !prev)} type="button" aria-haspopup="menu">
-      {items.title}{" "}
-     </button>
-     <Dropdown dropdown={dropdown} submenus={items.submenu} />
-    </>
-   ) : (
-            <Link to={`/${items.url}`}>
-        <li key={items.title}>{items.title}</li>
-    </Link>
-   )}
-  </li>
- );
+  return (
+    <li className="menu-items" ref={ref}>
+      {items.submenu ? (
+        <>
+          <button
+            aria-expanded={dropdown ? "true" : "false"}
+            onClick={() => setDropdown((prev) => !prev)}
+            type="button"
+            aria-haspopup="menu"
+          >
+            {items.title}{" "}
+          </button>
+          <Dropdown dropdown={dropdown} submenus={items.submenu} />
+        </>
+      ) : (
+        <Link to={`/${items.url}`} className="menu-item-link" key={items.title}>
+          {items.title}
+        </Link>
+      )}
+    </li>
+  );
 };
 
-export default MenuItems
+export default MenuItems;
